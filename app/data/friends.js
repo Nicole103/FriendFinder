4c275a5814abb1a34edadcda3ac49fffc8c467da
Nicole Heersink <nheersink8@gmail.com>
2020-01-12 13:11:10 -0700

    updating survey questions and friend data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,9 @@
 var friendData =[
     {
-        name: "Mario"
-        Photo: ""
-        Scores: []
+        name: "Nicole Heersink",
+        Photo: "https://media.licdn.com/dms/image/C4E03AQG_3FUfBZLJNw/profile-displayphoto-shrink_200_200/0?e=1583366400&v=beta&t=0M3JuMEhfGOyQmJOXU_TSFsDSQK_ZQhkiSxjK8B9sKE",
+        Scores: ["2", "1", "3", "4", "5", "4", "3", "5", "2", "3"]
     }
 ]
+
+module.exports = friendData;
