7df40f711584f341350767313f138f6cd6c48cd9
Nicole Heersink <nheersink8@gmail.com>
2020-01-12 22:31:15 -0700

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,17 +2,17 @@ var friendData =[
     {
         name: "Jim",
         photo: "https://vignette.wikia.nocookie.net/theoffice/images/9/9a/Jim.jpg/revision/latest?cb=20170701084550",
-        scores: [2,1,3,4,5,4,3,5,2,3]
+        scores: ["2","1","3","4","5","4","3","5","2","3"]
     },
     {
         name: "Pam",
         photo: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Pam_Beesley.jpg/220px-Pam_Beesley.jpg",
-        scores: [2,2,3,3,5,4,3,5,2,3]
+        scores: ["2","2","3","3","5","4","3","5","2","3"]
     },
     {
         name: "Michael",
         photo: "https://vignette.wikia.nocookie.net/theoffice/images/0/02/Michael_Scott.jpg/revision/latest?cb=20170701090332",
-        scores: [1,1,1,1,1,1,1,1,1,1]
+        scores: ["1","1","1","1","1","1","1","1","1","1",]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,47 +3,48 @@ var friendData = require("../data/friends.js");
 
 
 // ROUTING
-module.exports = function(app) {
- 
-    // API GET Requests
-    app.get("/api/friends", function(req, res) {
-     res.json(friendData);
+module.exports = function (app) {
+
+  // API GET Requests
+  app.get("/api/friends", function (req, res) {
+    res.json(friendData);
 
   });
 
   // API POST Requests  
-  app.post("/api/friends", function(req, res) {
-    
+  app.post("/api/friends", function (req, res) {
+
     //collecting the information from the submitted survey
     var newFriend = req.body;
     var newFriendScores = newFriend.scores;
 
-    var matchName ="";
-    var matchPic = "";
+    var matchName = " ";
+    var matchPic = " ";
 
     var maxDiff = 51;
 
     // loop through all friend sums to comapair scores
-    for(var i=0; i < friendData.length; i++){
-        
+    for (var i = 0; i < friendData.length; i++) {
+
       var scoreDiff = 0;
 
-      for(var j=0; j < newFriendScores.length; j++){
+      for (var j = 0; j < newFriendScores.length; j++) {
 
-        scoreDiff += Math.abs(parseInt(friendData[i].scores[j] - newFriendScores[j]));
-        }
-        
-    }
-    if (scoreDiff < maxDiff){
-      maxDiff = scoreDiff;
-      matchName = friendData[i].name;
-      matchPic = friendData[i].photo;
+        scoreDiff += Math.abs(friendData[i].scores[j] - newFriendScores[j]);
+      }
+
+
+      if (scoreDiff < maxDiff) {
+        maxDiff = scoreDiff;
+        matchName = friendData[i].name;
+        matchPic = friendData[i].photo;
+      }
     }
 
-     
+
     friendData.push(newFriend);
-    
-    res.json({ matchName: matchName, matchPic: matchPic});
+
+    res.json({ matchName: matchName, matchPic: matchPic });
 
   });
 };
